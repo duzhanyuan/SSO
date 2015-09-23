1e54daa31a53d959e208ede1c9ee6c0b0cbc152d
Jian Huang <huangjian1993@gmail.com>
2015-09-23 17:29:11 +0800

    add GetAllData for monitor

--- src/service/monitor/monitor.go
+++ src/service/monitor/monitor.go
@@ -37,3 +37,26 @@ func IncrCount() {
 	atomic.AddInt64(&count, 1)
 	fmt.Println("now count is: ", count)
 }
+
+func GetAllData() []int64 {
+	data := []int64{}
+	mu.Lock()
+	for it := cntList.Front(); it != nil; it = it.Next() {
+		data = append(data, it.Value)
+	}
+	mu.Unlock()
+	return data
+}
+
+/*func GetData(seconds int64) []int64 {*/
+//data := []int64{}
+//seconds = seconds / 5
+//for it := cntList.Back(); it != nil; it = it.Prev() {
+//data = append(data, it.Value)
+//seconds = seconds - 1
+//if seconds == 0 {
+//break
+//}
+//}
+//return data
+/*}*/
