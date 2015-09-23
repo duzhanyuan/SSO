7b42051fda5c47952d5d9adeefe30003243c31d5
Jian Huang <huangjian1993@gmail.com>
2015-09-24 04:26:54 +0800

    fix bugs

--- src/http/web.go
+++ src/http/web.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"webservice/handler"
+	"webservice/model"
 )
 
 func regRouter(router *gin.Engine) {
@@ -13,9 +18,29 @@ func regRouter(router *gin.Engine) {
 func main() {
 	router := gin.Default()
 	regRouter(router)
-	listenAddr := "127.0.0.1:8001"
+	//listenAddr := "127.0.0.1:10000/user/register_service"
+	addr := "http://127.0.0.1:10000/user/register_service"
+	resp, err := http.PostForm(addr, url.Values{"name": {"test"}})
+	type Data struct {
+		Key string `json:"key"`
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	err := http.ListenAndServe(listenAddr, router)
+	data := Data{}
+	json.Unmarshal([]byte(string(body)), &data)
+	model.Service_key = ([]byte)(data.Key)
+
+	listenAddr := "0.0.0.0:9999"
+	err = http.ListenAndServe(listenAddr, router)
 	if err != nil {
 		panic(err)
 	}
